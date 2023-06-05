d6f6e458300352f144958f0ec16742e03cde2d4a
lucastestsstuff <135617917+lucastestsstuff@users.noreply.github.com>
2023-06-05 20:53:24 +0800

    Delete VS autofarm.lua

--- VS autofarm.lua
+++
@@ -1,7 +0,0 @@
-local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
-for i, v in pairs(game:GetService("Workspace").Pickups:GetChildren())do
-if v:IsA("MeshPart")then
-rootPart.CFrame = v.CFrame wait()do
-end
-end
-end
